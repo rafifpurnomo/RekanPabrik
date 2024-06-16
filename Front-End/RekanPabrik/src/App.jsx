6a700dff434dc1ac100c86e3e7ceee22735a2722
unknown <rafif.purnomo@gmail.com>
2024-06-16 20:44:06 +0700

    Add Front End

---
+++ Front-End/RekanPabrik/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePelamar from "./pages/HomePelamar";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/HomePelamar" element={<HomePelamar/>}/>
+        <Route path="/Login" element={<LoginPage/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ Front-End/RekanPabrik/src/pages/LoginPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoginPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Ini login page</h1>
+    </div>
+  );
+}
+
+export default LoginPage;
